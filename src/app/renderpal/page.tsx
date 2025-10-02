a08efcfe43de9a892db9cfe6c18459c16ae17b27
Aditya <adityaarrofi806@gmail.com>
2025-10-02 21:20:19 +0700

    renderpal

--- src/app/renderpal/page.tsx
+++ src/app/renderpal/page.tsx
@@ -1,8 +1,41 @@
 export default function RenderPal() {
-  return (
-    <div className="min-h-screen text-center p-12">
-        <h1 className="text-3xl font-bold">RenderPal Cost Estimator</h1>
-        <p className="mt-2 text-white">Render Cepat Mulai 100 Rupiah Tanpa Ganggu Proyek Kamu!</p>
+    return (
+
+<div className="bg-[#212121] text-white">
+  <div className="p-12 text-center">
+    <h1 className="text-3xl font-bold">RenderPal Cost Estimator</h1>
+    <p className="mt-2">Render Cepat Mulai 100 Rupiah Tanpa Ganggu Proyek Kamu!</p>
+  </div>
+
+<div className="j flex justify-center">
+    <div className="flex flex-col justify-center bg-[#2E2E2E] p-20 w-200 rounded-4xl">
+    <select name="" id="" className="m-2 w-full rounded-xl bg-white p-3 text-gray-950">
+      <option value="1">halo</option>
+      <option value="2">halo</option>
+    </select>
+    <div>
+      <select name="" id="" className="m-2 w-full rounded-xl bg-white p-3 text-gray-950">
+        <option value="1">halo</option>
+        <option value="2">halo</option>
+      </select>
     </div>
-  )
+    <div>
+      <select name="" id="" className="m-2 w-full rounded-xl bg-white p-3 text-gray-950">
+        <option value="1">halo</option>
+        <option value="2">halo</option>
+      </select>
+    </div>
+    <div>
+      <select name="" id="" className="m-2 w-full rounded-xl bg-white p-3 text-gray-950">
+        <option value="1">halo</option>
+        <option value="2">halo</option>
+      </select>
+    </div>
+  </div>
+</div>
+
+</div>
+
+        
+    )
 }
