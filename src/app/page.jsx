6b3e5d3b55aa11eeb2db555d2b1937509ed87eee
Aditya <adityaarrofi806@gmail.com>
2025-10-13 21:27:45 +0700

    move

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,7 @@
 'use client'
 import Image from "next/image";
 import Link from "next/link";
-import ModelViewer from '../../components/modelviewer'
-import HeroSection from "../../components/herosection";
+
 
 
 
