c2c9c18c86c729e5f343946d845728e68b3fde84
Aditya <adityaarrofi806@gmail.com>
2025-10-16 00:28:15 +0700

    asset

--- src/app/api/ftechdata/route.js
+++ src/app/api/ftechdata/route.js
@@ -8,21 +8,27 @@ dotenv.config({
 
 
 export async function GET() {
-    const WooUrl = `${process.env.WC_URL}/wp-json/wc/v3/products`
+    const WooUrl = `${process.env.WC_URL}/wp-json/wc/v3/products?exclude=3013,2619,2488,2486,2494`
     const key = process.env.WC_CONSUMER_KEY;
     const secret = process.env.WC_CONSUMER_SECRET;
 
 
-    const response = await fetch(`${WooUrl}?consumer_key=${key}&consumer_secret=${secret}`);
+    const response = await fetch(`${WooUrl}&consumer_key=${key}&consumer_secret=${secret}&per_page=50`);
     const body = await response.json()
     const product = body.map(p => ({
         name: p.name,
         price: p.price,
         image: p.images[0] ?.src || '',
+        store: p.store.shop_name,
     }));
 
     const file = 'woo-response.json'
     return Response.json(product);
+    // return new Response(JSON.stringify(body, null, 2), {
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     }
+    // });
     // writeFileSync(file, JSON.stringify(product, null, 2), "utf8");
 
 }
